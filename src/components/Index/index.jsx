8b5d4efaec013498db68319da7271d705ad391cb
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-29 22:11:51 +0800

    将导航菜单改为使用row、col栅格组件，增加轮播图组件

--- src/components/Index/index.jsx
+++ src/components/Index/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-// Layout布局；Menu导航菜单；Input输入框；Button按钮
-import { Layout,Menu, Breadcrumb,Input,Button  } from 'antd'
-import { UnorderedListOutlined } from '@ant-design/icons'; //TODO新版本antd，按需引入
-import './index.css'
+import React from "react";
+// Layout布局；Row, Col栅格组件；Menu导航菜单；Input输入框；Button按钮；Carousel跑马灯
+import { Layout, Row, Col, Menu, Input, Button, Carousel } from "antd";
+import { UnorderedListOutlined, SearchOutlined } from "@ant-design/icons"; //TODO新版本antd，按需引入
+import "./index.css";
 
 export default function index() {
   const { Header, Footer, Content } = Layout;
@@ -12,87 +12,173 @@ export default function index() {
     <div>
       <Layout>
         {/* 头部 */}
-        <Header style={{ position: 'fixed', zIndex: 1, width: '100%',backgroundColor:"#fff" }}>
-          {/* horizontal 菜单水平
+        <Header
+          style={{
+            position: "fixed",
+            zIndex: 1,
+            width: "100%",
+            backgroundColor: "#fff",
+            float: "left",
+          }}
+        >
+          <Row gutter={16} type="flex" justify="center" >
+            <Col>
+              {/* horizontal 菜单水平
               theme 主题
               defaultSelectedKeys 初始选中的菜单项 key 数组 
               TODO 可以使用flex布局的属性justifyContent居中*/}
-          <Menu
-            theme="light"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ height: '64px',justifyContent:"center" }}
-          >
-            <Menu.Item style={{paddingTop:'20px',marginRight:'24px',paddingBottom:'9px'}}>
-              <img src='../../../logo.png' alt='加载中' />
-            </Menu.Item>
-            <SubMenu
-              title={
-                <span>
-                  全部课程
-                </span>
-              }
-              style={{backgroundColor:"#4aa1f0",color:"white"}}
+              <Menu
+                theme="light"
+                mode="horizontal"
+                defaultSelectedKeys={["2"]}
+                style={{ height: "64px" }}
+              >
+                <Menu.Item
+                  style={{
+                    paddingTop: "20px",
+                    marginRight: "24px",
+                    paddingBottom: "9px",
+                  }}
+                >
+                  <img src="../../../logo.png" alt="加载中" />
+                </Menu.Item>
+                <SubMenu
+                  title={
+                    <span>
+                      全部课程
+                      <UnorderedListOutlined style={{ marginLeft: "5px" }} />
+                    </span>
+                  }
+                  style={{
+                    backgroundColor: "#4aa1f0",
+                    color: "white",
+                    width: "116px",
+                    textAlign: "center",
+                  }}
+                >
+                  <Menu.Item key="setting:1">政策法律法规</Menu.Item>
+                  <Menu.Item key="setting:1">政策法律法规</Menu.Item>
+                  <Menu.Item key="setting:1">政策法律法规</Menu.Item>
+                  <Menu.Item key="setting:1">政策法律法规</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="1">首页</Menu.Item>
+                <Menu.Item key="2">图书馆</Menu.Item>
+                <Menu.Item key="3">环境法宝</Menu.Item>
+                <Menu.Item key="4">名师讲堂</Menu.Item>
+                <Menu.Item key="5">热门资讯</Menu.Item>
+              </Menu>
+            </Col>
+            {/* TODO 解决Search输入框无法垂直居中 */}
+            <Col
+              style={{ display: "flex", alignItems: "center", width: "200px" }}
             >
-              <Menu.Item key="setting:1">政策法律法规</Menu.Item>
-              <Menu.Item key="setting:1">政策法律法规</Menu.Item>
-              <Menu.Item key="setting:1">政策法律法规</Menu.Item>
-              <Menu.Item key="setting:1">政策法律法规</Menu.Item>
-            </SubMenu>
-            <UnorderedListOutlined />
-            <Menu.Item key="1">首页</Menu.Item>
-            <Menu.Item key="2">图书馆</Menu.Item>
-            <Menu.Item key="3">环境法宝</Menu.Item>
-            <Menu.Item key="4">名师讲堂</Menu.Item>
-            <Menu.Item key="5">热门资讯</Menu.Item>
-            <Menu.Item key="6" style={{ width: 200, alignItems:"center !important" }}>
-              {/* TODO 输入框没有垂直居中 */}
-              <Search
-                placeholder="课程、班级、..."
-                onSearch={value => console.log(value)}
+              <Search placeholder="课程、班级、..." />
+            </Col>
+            <Col>
+              <span
+                style={{
+                  color: "#ffa537",
+                  textAlign: "center",
+                  fontSize: "15px",
+                  fontWeight: 600,
+                  overflow: "hidden",
+                  textOverflow: "ellipsis ",
+                  cursor: "pointer",
+                }}
+              >
+                VIP畅学
+              </span>
+            </Col>
+            {/* TODORow全局align="middle" 对imh无效  */}
+            <Col style={{ display: "flex", alignItems: "center"}}>
+              <img
+                src="../../../download.png"
+                alt="加载中"
+                style={{ width: "13px", height: "21px" }}
               />
-            </Menu.Item>
-            <Menu.Item key="7" >
-              <span style={{
-                color: "#ffa537",
-                textAlign: "center",
-                fontSize: "15px",
-                fontWeight: 600,
-                overflow: "hidden",
-                textOverflow: "ellipsis ",
-                cursor: "pointer",
-            }}>VIP畅学</span></Menu.Item>
-            <Menu.Item key="8">
-              <img src='../../../download.png' alt='加载中' style={{width:"13px",height:'21PX'}}/>
               <span>下载APP</span>
-            </Menu.Item>
-            <Menu.Item key="9">
-              <img src='../../../xinrenyouli.png' style={{position:"absolute",top:"-2.5px",left:"-2.5px"}}/>
+            </Col>
+            <Col>
+              <img
+                src="../../../xinrenyouli.png"
+                style={{ position: "absolute", top: "8px", left: "-4px" }}
+              />
               <span>登录/注册</span>
-            </Menu.Item>
-            <Menu.Item key="10">
-              <Button style={{
-                borderRadius: "15px",
-                fontSize: "14px",
-                backgroundColor: "#1fa2f8",
-                color: "#fff",
-                cursor: "pointer",
-              }}>查询证书</Button>
-            </Menu.Item>
-          </Menu>
+            </Col>
+            <Col>
+              <Button
+                style={{
+                  borderRadius: "15px",
+                  fontSize: "14px",
+                  backgroundColor: "#1fa2f8",
+                  color: "#fff",
+                  cursor: "pointer",
+                }}
+              >
+                查询证书
+              </Button>
+            </Col>
+          </Row>
         </Header>
         {/* 内容区 */}
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+        <Content style={{ marginTop: 64 }}>
+          {/* 轮播图 */}
+          <Carousel autoplay>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1678758914412.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1678859575872.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1685946053778.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1678783346954.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1682565542094.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1682565562660.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1654849687735.jpg"
+              />
+            </div>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="https://data.cei.net.cn/images/product/1654849878917.jpg"
+              />
+            </div>
+          </Carousel>
         </Content>
         {/* 底部 */}
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
       </Layout>
     </div>
-  )
+  );
 }
