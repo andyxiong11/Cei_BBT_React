07f7a202f63ea2379019fbaf894d7805e2f16095
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-18 23:08:49 +0800

    将培训专栏转写为展示传入的数据

--- src/components/Floor/index.jsx
+++ src/components/Floor/index.jsx
@@ -1,9 +1,6 @@
 import "./index.css";
-export default function Floor() {
-  const text = [
-    '第一集：环境综合服务市场占不住？业务实践大力拓展市场渠道！',
-    '第二集：你真的会做管家业务？业务以技术为主，技术以人才为主！',
-    '第三集：环保管家如何对待客户？专家给你5大利器！']
+export default function Floor(props) {
+  const {img,courseTop,courseBottom,message,issue,imgBanner} = props
   return (
     <div style={{
             width:"1230px",
@@ -18,12 +15,16 @@ export default function Floor() {
         {/* flexDirection从上到下  */}
         {/* TODO父元素给高度后，justifyContent:"space-between"才有效 */}
         <div style={{display:"flex",flexDirection:"column",justifyContent:"space-between"}}>
-          <div>
-            <img style={{width:"210px",height:"288px",borderRadius: "6px"}} src="https://data.cei.net.cn/images/advertisement/1650421246187.jpg"/>
-          </div>
-          <div>
-            <img style={{width:"210px",height:"288px",borderRadius: "6px"}} src="https://data.cei.net.cn/images/advertisement/1646206522403.jpg"/>
-          </div>
+          {
+            // TODOmap方法报错不能为空，已解决
+            img&&img.map((item)=>{
+              return (
+                <div>
+                  <img style={{width:"210px",height:"288px",borderRadius: "6px"}} src={item}/>
+                </div>
+              )
+            })
+          }
         </div>
         {/* 右边内容区 */}
         <div style={{width:"63vw",backgroundColor:"white",display:"flex"}}>
@@ -47,109 +48,63 @@ export default function Floor() {
             <div style={{height: "540px",display:"flex",flexDirection:"column",justifyContent:"space-between"}}>
               {/* 上部分内容 */}
               <div style={{display:"flex",justifyContent:"space-between"}}>
-                {/* TODO每个盒子给百分比宽度,justifyContent:"space-between" */}
-                <div style={{width:"31%",height:"240px",position:"relative"}}>
-                  <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
-                  <div>
-                    {/* TODO图片给100%宽度，按比例缩放 */}
-                    <img style={{width:"100%",borderRadius: "5px"}} src="https://data.cei.net.cn/images/product/1624341234427.jpg"/>
-                  </div>
-                  <div style={{width: "40px",height: "40px",top: "88px",right:"0px",position: "absolute"}}>
-                    <img style={{width:"100%"}} src="../../../jingpin.png"/>
-                  </div>
-                  <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>{text[0]}</div>
-                  <div>
-                    <img src="../../../huo.png"/>
-                    1727人已购买
-                  </div>
-                  <div style={{display:"flex",justifyContent:"space-between"}}>
-                    <span>主讲人 西尔</span>
-                    <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
-                  </div>
-                </div>
-                {/* TODO每个盒子给百分比宽度,justifyContent:"space-between" */}
-                <div style={{width:"31%",height:"240px",position:"relative"}}>
-                  <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
-                  <div>
-                    {/* TODO图片给100%宽度，按比例缩放 */}
-                    <img style={{width:"100%",borderRadius: "5px"}} src="https://data.cei.net.cn/images/product/1624341234427.jpg"/>
-                  </div>
-                  <div style={{width: "40px",height: "40px",top: "88px",right:"0px",position: "absolute"}}>
-                    <img style={{width:"100%"}} src="../../../jingpin.png"/>
-                  </div>
-                  <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>{text[1]}</div>
-                  <div>
-                    <img src="../../../huo.png"/>
-                    1727人已购买
-                  </div>
-                  <div style={{display:"flex",justifyContent:"space-between"}}>
-                    <span>主讲人 西尔</span>
-                    <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
-                  </div>
-                </div>
-                {/* TODO每个盒子给百分比宽度,justifyContent:"space-between" */}
-                <div style={{width:"31%",height:"240px",position:"relative"}}>
-                  <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
-                  <div>
-                    {/* TODO图片给100%宽度，按比例缩放 */}
-                    <img style={{width:"100%",borderRadius: "5px"}} src="https://data.cei.net.cn/images/product/1624341234427.jpg"/>
-                  </div>
-                  <div style={{width: "40px",height: "40px",top: "88px",right:"0px",position: "absolute"}}>
-                    <img style={{width:"100%"}} src="../../../jingpin.png"/>
-                  </div>
-                  <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>{text[2]}</div>
-                  <div>
-                    <img src="../../../huo.png"/>
-                    1727人已购买
-                  </div>
-                  <div style={{display:"flex",justifyContent:"space-between"}}>
-                    <span>主讲人 西尔</span>
-                    <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
-                  </div>
-                </div>
+                {
+                  // TODOmap方法报错不能为空，已解决
+                  courseTop&&courseTop.map((item)=>{
+                    return (
+                      // TODO每个盒子给百分比宽度,justifyContent:"space-between"
+                      <div style={{width:"31%",height:"240px",position:"relative"}}>
+                        <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
+                        <div>
+                          {/* TODO图片给100%宽度，按比例缩放 */}
+                          <img style={{width:"100%",borderRadius: "5px"}} src={item.img}/>
+                        </div>
+                        <div style={{width: "40px",height: "40px",top: "88px",right:"0px",position: "absolute"}}>
+                          <img style={{width:"100%"}} src="../../../jingpin.png"/>
+                        </div>
+                        <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>{item.title}</div>
+                        <div>
+                          <img src="../../../huo.png"/>
+                          1727人已购买
+                        </div>
+                        <div style={{display:"flex",justifyContent:"space-between"}}>
+                          <span>主讲人 西尔</span>
+                          <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
               </div>
               {/* 下部分内容 */}
               <div style={{display:"flex",justifyContent:"space-between"}}>
-                {/* TODO每个盒子给百分比宽度,justifyContent:"space-between" */}
-                <div style={{width:"48%",position:"relative"}}>
-                  <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
-                  <div>
-                    {/* TODO图片给100%宽度，按比例缩放 */}
-                    <img style={{width:"100%",height:"100%",orderRadius: "5px"}} src="https://data.cei.net.cn/images/product/1678783456408.jpg"/>
-                  </div>
-                  <div style={{width: "40px",height: "40px",top: "158px",right:"0px",position: "absolute"}}>
-                    <img style={{width:"100%"}} src="../../../jingpin.png"/>
-                  </div>
-                  <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>《生态环境综合服务（生态环保管家）及延伸》在线培训班</div>
-                  <div>
-                    <img src="../../../huo.png"/>
-                    1727人已购买
-                  </div>
-                  <div style={{display:"flex",justifyContent:"space-between"}}>
-                    <span>主讲人 西尔</span>
-                    <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
-                  </div>
-                </div>
-                {/* TODO每个盒子给百分比宽度,justifyContent:"space-between" */}
-                <div style={{width:"48%",position:"relative"}}>
-                  <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
-                  <div>
-                    {/* TODO图片给100%宽度，按比例缩放 */}
-                    <img style={{width:"100%",height:"100%",borderRadius: "5px"}} src="https://data.cei.net.cn/images/product/1623823524215.jpg"/>
-                  </div>
-                  <div style={{width: "40px",height: "40px",top: "158px",right:"0px",position: "absolute"}}>
-                    <img style={{width:"100%"}} src="../../../jingpin.png"/>
-                  </div>
-                  <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>第三方环保服务业专项技能训练</div>
-                  <div>
-                    <img src="../../../huo.png"/>
-                    1727人已购买
-                  </div>
-                  <div style={{display:"flex",justifyContent:"space-between"}}>
-                    <span>主讲人 西尔</span>
-                    <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
-                  </div>
-                </div>
+                {
+                  // TODOmap方法报错不能为空，已解决
+                  courseBottom&&courseBottom.map((item)=>{
+                    return (
+                      // TODO每个盒子给百分比宽度,justifyContent:"space-between"
+                      <div style={{width:"48%",position:"relative"}}>
+                      <div style={{width: "45px",height: "25px",lineHeight:"25px",textAlign:"center",color:"#fff",backgroundColor: "rgba(0,0,0,.3)",right: "6px",top: "6px",position: "absolute"}}>在线</div>
+                      <div>
+                        {/* TODO图片给100%宽度，按比例缩放 */}
+                        <img style={{width:"100%",height:"100%",orderRadius: "5px"}} src={item.img}/>
+                      </div>
+                      <div style={{width: "40px",height: "40px",top: "158px",right:"0px",position: "absolute"}}>
+                        <img style={{width:"100%"}} src="../../../jingpin.png"/>
+                      </div>
+                      <div className="SL" style={{height: "43px",fontSize: "15px",fontWeight: "600",marginTop: "5px"}}>{item.title}</div>
+                      <div>
+                        <img src="../../../huo.png"/>
+                        1727人已购买
+                      </div>
+                      <div style={{display:"flex",justifyContent:"space-between"}}>
+                        <span>主讲人 西尔</span>
+                        <span style={{fontSize: "20px",color: "#f40"}}>￥0</span>
+                      </div>
+                    </div>
+                    )
+                  })
+                }
               </div>
             </div>
           </div>
@@ -165,30 +120,21 @@ export default function Floor() {
                   </div>
                   <div style={{fontSize: ".85rem"}}>更多{'>'}</div>
                 </div>
-                <div style={{marginBottom: "10px"}}>
-                  <div className="SL" style={{height: "50px",lineHeight: "25px",fontSize: ".85rem"}}>
-                    生态环境部固体废物与化学品司有关负责人就《关于进一步加强危险废物规范化环境管理有关工作的通知》答记者问
-                  </div>
-                  <div style={{height: "25px",lineHeight: "25px",fontSize: ".7rem"}}>
-                    2023-11-10
-                  </div>
-                </div>
-                <div style={{marginBottom: "10px"}}>
-                  <div className="SL" style={{height: "50px",lineHeight: "25px",fontSize: ".85rem"}}>
-                    生态环境部固体废物与化学品司有关负责人就《关于进一步加强危险废物规范化环境管理有关工作的通知》答记者问
-                  </div>
-                  <div style={{height: "25px",lineHeight: "25px",fontSize: ".7rem"}}>
-                    2023-11-10
-                  </div>
-                </div>
-                <div style={{marginBottom: "10px",borderBottom: "1px solid #ddd"}}>
-                  <div className="SL" style={{height: "50px",lineHeight: "25px",fontSize: ".85rem"}}>
-                    生态环境部固体废物与化学品司有关负责人就《关于进一步加强危险废物规范化环境管理有关工作的通知》答记者问
-                  </div>
-                  <div style={{height: "25px",lineHeight: "25px",fontSize: ".7rem"}}>
-                    2023-11-10
-                  </div>
-                </div>
+                {
+                  // TODOmap方法报错不能为空，已解决
+                  message&&message.map((item)=>{
+                    return (
+                      <div style={{marginBottom: "10px"}}>
+                        <div className="SL" style={{height: "50px",lineHeight: "25px",fontSize: ".85rem"}}>
+                          {item}
+                        </div>
+                        <div style={{height: "25px",lineHeight: "25px",fontSize: ".7rem"}}>
+                          2023-11-10
+                        </div>
+                      </div>
+                    )
+                  })
+                }
             </div>
             {/* 下部分 */}
             <div style={{marginTop:"15px",padding:"0 0.5vw"}}>
@@ -196,7 +142,8 @@ export default function Floor() {
                 <div style={{width: "7vw"}}>热门问答</div>
                 <div style={{width: "7vw"}}>环境法宝</div>
               </div>
-              <div style={{height: "50px"}}>电厂冷却用的温排水是否属于清净下水?</div>
+              {/* TODOissue报错不能为空，已解决 */}
+              <div style={{height: "50px"}}>{issue&&issue.quesiton}</div>
               <div style={{display:"flex",justifyContent:"space-between"}}>
                 <div style={{display:"flex"}}>
                   <div style={{height: "45px",width: "45px"}}>
@@ -214,8 +161,9 @@ export default function Floor() {
                 </div>
               </div>
               <div style={{height: "110px",position:"relative"}}>
+                {/* TODOissue报错不能为空，已解决 */}
                 <div style={{width:"100%",height: "90px",lineHeight: "22px",fontSize: "12px",backgroundColor:"#fffaf2",overflow:"hidden"}}>
-                  在排放标准中，一般不使用“清净下水”这一术语。但在日常环境管理中，一般认为清净下水包括温排水、锅炉循环水等。考虑到这类清净下水通常为循环水，运行中常需加入阻垢剂、杀菌剂、杀藻剂等，可能导致循环水化学需氧量、总磷超标，因此，多数排放标准将此类废水纳入管控范围，要求处理达标后方可排放。
+                  {issue&&issue.answer}
                 </div>
                 {/* TODO需要绝对定位才能使用right：0 */}
                 <div style={{color: "#71a8ef",right:"0",position:"absolute"}}>查看</div>
@@ -233,8 +181,10 @@ export default function Floor() {
       {/* 下部分 */}
       <div style={{height: "200px",display:"flex",justifyContent:"space-between",marginTop:"2vh"}}>
           {/* TODO图片给100%高度，才会按照父盒子高度缩放 */}
-          <img style={{height:"100%",width:"49%",borderRadius: "3px"}} src="https://data.cei.net.cn/images/advertisement/1646271226382.jpg"></img>
-          <img style={{height:"100%",width:"49%",borderRadius: "3px"}} src="https://data.cei.net.cn/images/advertisement/1646271682744.jpg"></img>
+          {/* TODO这里不用遍历，会影响flex布局 */}
+          {/* TODOimgBanner报错不能为空，已解决 */}
+          <img style={{height:"100%",width:"49%",borderRadius: "3px"}} src={imgBanner&&imgBanner[0]}></img>
+          <img style={{height:"100%",width:"49%",borderRadius: "3px"}} src={imgBanner&&imgBanner[1]}></img>
       </div>
     </div>
   )
