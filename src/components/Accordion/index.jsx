8d17b6e6c653dde3d93f3ed0a78da3da0e3ad921
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-19 21:58:43 +0800

    水平手风琴使用antd折叠面板改造，未完成

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,5 +1,9 @@
+import { Collapse } from 'antd';
 import { useState } from "react";
 import './index.css'
+
+const { Panel } = Collapse;
+
 export default function Accordion() {
   const [flag,setFlag] = useState(true)
   const handleMouseEnter  = (e) => {
@@ -8,54 +12,108 @@ export default function Accordion() {
     console.log(flag);
   }
   return (
-    <div style={{ width: "1260px",
-                  height: "270px",
-                  margin: "50px auto",
-                  border: "1px solid red",
-                  position:"relative",
-                  overflow:"hidden"
-                }}>
-          <ul style={{position:"absolute",width:"1980px",listStyle:"none"}}>
-            <li className="list" 
-                style={{ 
-                  // TODO图片位置写法
-                  backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-                }} onMouseEnter={handleMouseEnter}></li>
-            <li className="list_long" 
-                style={{ 
-                  backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
-                }}></li>
-            <li className="list" 
-                style={{ 
-                  marginLeft: flag ? "-660px" : "0px",
-                  backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-                }} onMouseEnter={handleMouseEnter}></li>
-            <li className="list_long" 
-                style={{ 
-                  backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
-                }}></li>
-            <li className="list" 
-                style={{ 
-                  marginLeft: flag ? "-660px" : "0px",
-                  backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-                }} onMouseEnter={handleMouseEnter}></li>
-            <li className="list_long" 
-                style={{ 
-                  backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
-                }}></li>
-            {/* {
-              img.map(()=>{
-                  // <li style={backgroundImage:"url(images/" + i + ".jpg)"}></li>
-                  return (<li style={{
-                                width: "660px",
-                                height: "270px",
-                                float:"left",
-                                backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
-                              }} onMouseEnter={handleMouseEnter}></li>
-                  )
-              })
-            } */}
-        </ul>
-    </div>
+    <Collapse accordion defaultActiveKey={1}  style={{display:"flex",justifyContent:"center"}}>
+      <Panel 
+        showArrow={false}
+        key="1"
+        style={{
+          display:"flex",
+          height:"270px",
+          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
+          backgroundRepeat:"no-repeat"
+        }}>
+        <div style={{
+          width:"660px",
+          height:"270px", 
+          margin:"-16px -16px 0 -16px",
+          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
+          backgroundRepeat:"no-repeat"
+        }}/>
+      </Panel>
+      <Panel 
+        showArrow={false} 
+        key="2"
+        style={{
+          display:"flex",
+          height:"270px",
+          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
+          backgroundRepeat:"no-repeat"
+        }}>
+        <div style={{
+          width:"660px",
+          height:"270px", 
+          margin:"-16px -16px 0 -16px",
+          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
+          backgroundRepeat:"no-repeat"
+        }}/>
+      </Panel>
+      <Panel 
+        showArrow={false} 
+        key="3"
+        style={{
+          display:"flex",
+          height:"270px",
+          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
+          backgroundRepeat:"no-repeat"
+        }}>
+        <div style={{
+          width:"660px",
+          height:"270px", 
+          margin:"-16px -16px 0 -16px",
+          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
+          backgroundRepeat:"no-repeat"
+        }}/>
+      </Panel>
+    </Collapse>
+
+    // <div style={{ width: "1260px",
+    //               height: "270px",
+    //               margin: "50px auto",
+    //               border: "1px solid red",
+    //               position:"relative",
+    //               overflow:"hidden"
+    //             }}>
+    //       <ul style={{position:"absolute",width:"1980px",listStyle:"none"}}>
+    //         <li className="list" 
+    //             style={{ 
+    //               // TODO图片位置写法
+    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
+    //             }} onMouseEnter={handleMouseEnter}></li>
+    //         <li className="list_long" 
+    //             style={{ 
+    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
+    //             }}></li>
+    //         <li className="list" 
+    //             style={{ 
+    //               marginLeft: flag ? "-660px" : "0px",
+    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
+    //             }} onMouseEnter={handleMouseEnter}></li>
+    //         <li className="list_long" 
+    //             style={{ 
+    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
+    //             }}></li>
+    //         <li className="list" 
+    //             style={{ 
+    //               marginLeft: flag ? "-660px" : "0px",
+    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
+    //             }} onMouseEnter={handleMouseEnter}></li>
+    //         <li className="list_long" 
+    //             style={{ 
+    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
+    //             }}></li>
+    //         {/* {
+    //           img.map(()=>{
+    //               // <li style={backgroundImage:"url(images/" + i + ".jpg)"}></li>
+    //               return (<li style={{
+    //                             width: "660px",
+    //                             height: "270px",
+    //                             float:"left",
+    //                             backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
+    //                           }} onMouseEnter={handleMouseEnter}></li>
+    //               )
+    //           })
+    //         } */}
+    //     </ul>
+    // </div>
   )
 }
