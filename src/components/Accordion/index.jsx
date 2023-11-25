7129f30c85e54be207ac3a18a137701492d89adf
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-25 23:20:21 +0800

    水平手风琴完成，animate动画有问题

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,119 +1,57 @@
-import { Collapse } from 'antd';
+import { Row, Col } from 'antd';
 import { useState } from "react";
 import './index.css'
-
-const { Panel } = Collapse;
+import slide_one from '../../../public/shizhanlianbin.jpg'
+import slide_two from '../../../public/jianbinpeixun.jpg'
+import slide_three from '../../../public/zhishijingsai.jpg'
+import slide_long_one from '../../../public/shizhanlianbin_long.jpg'
+import slide_long_two from '../../../public/jianbinpeixun_long.jpg'
+import slide_long_three from '../../../public/zhishijingsai_long.jpg'
 
 export default function Accordion() {
-  const [flag,setFlag] = useState(true)
-  const handleMouseEnter  = (e) => {
-    console.log(e.target);
-    setFlag(!flag)
-    console.log(flag);
+  const [drawerData] = useState([
+    {num : 1 ,smallImg :slide_two , longImg : slide_long_two},
+    {num : 2 ,smallImg :slide_one , longImg : slide_long_one},
+    {num : 3 ,smallImg :slide_three , longImg : slide_long_three},
+  ])
+  function drawerClick(index) {
+    for(var i = 0;i < drawerData.length;i++){
+      let docMiddle = document.getElementById(i)
+         if(i <=  index){
+             /* docMiddle.animate({left:i*195+"px"},800);
+             setTimeout(() => {
+               docMiddle.style.left = i*195+'px'
+             }, 750) */
+             docMiddle.style.left = i*195+'px'
+         }else{
+            /* docMiddle.animate({left:(i+1)*195+390+"px"},800);
+            setTimeout(() => {
+                   docMiddle.style.left = (i+1)*195+390+'px'
+            }, 750); */
+            docMiddle.style.left = (i+1)*195+390+'px'
+         }
+    }
   }
   return (
-    <Collapse accordion defaultActiveKey={1}  style={{display:"flex",justifyContent:"center"}}>
-      <Panel 
-        showArrow={false}
-        key="1"
-        style={{
-          display:"flex",
-          height:"270px",
-          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-          backgroundRepeat:"no-repeat"
-        }}>
-        <div style={{
-          width:"660px",
-          height:"270px", 
-          margin:"-16px -16px 0 -16px",
-          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
-          backgroundRepeat:"no-repeat"
-        }}/>
-      </Panel>
-      <Panel 
-        showArrow={false} 
-        key="2"
-        style={{
-          display:"flex",
-          height:"270px",
-          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-          backgroundRepeat:"no-repeat"
-        }}>
-        <div style={{
-          width:"660px",
-          height:"270px", 
-          margin:"-16px -16px 0 -16px",
-          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
-          backgroundRepeat:"no-repeat"
-        }}/>
-      </Panel>
-      <Panel 
-        showArrow={false} 
-        key="3"
-        style={{
-          display:"flex",
-          height:"270px",
-          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-          backgroundRepeat:"no-repeat"
-        }}>
-        <div style={{
-          width:"660px",
-          height:"270px", 
-          margin:"-16px -16px 0 -16px",
-          backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
-          backgroundRepeat:"no-repeat"
-        }}/>
-      </Panel>
-    </Collapse>
-
-    // <div style={{ width: "1260px",
-    //               height: "270px",
-    //               margin: "50px auto",
-    //               border: "1px solid red",
-    //               position:"relative",
-    //               overflow:"hidden"
-    //             }}>
-    //       <ul style={{position:"absolute",width:"1980px",listStyle:"none"}}>
-    //         <li className="list" 
-    //             style={{ 
-    //               // TODO图片位置写法
-    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-    //             }} onMouseEnter={handleMouseEnter}></li>
-    //         <li className="list_long" 
-    //             style={{ 
-    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
-    //             }}></li>
-    //         <li className="list" 
-    //             style={{ 
-    //               marginLeft: flag ? "-660px" : "0px",
-    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-    //             }} onMouseEnter={handleMouseEnter}></li>
-    //         <li className="list_long" 
-    //             style={{ 
-    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
-    //             }}></li>
-    //         <li className="list" 
-    //             style={{ 
-    //               marginLeft: flag ? "-660px" : "0px",
-    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun.jpg'})`,
-    //             }} onMouseEnter={handleMouseEnter}></li>
-    //         <li className="list_long" 
-    //             style={{ 
-    //               backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`,
-    //             }}></li>
-    //         {/* {
-    //           img.map(()=>{
-    //               // <li style={backgroundImage:"url(images/" + i + ".jpg)"}></li>
-    //               return (<li style={{
-    //                             width: "660px",
-    //                             height: "270px",
-    //                             float:"left",
-    //                             backgroundImage:`url(${process.env.PUBLIC_URL + '/jianbinpeixun_long.jpg'})`
-    //                           }} onMouseEnter={handleMouseEnter}></li>
-    //               )
-    //           })
-    //         } */}
-    //     </ul>
-    // </div>
+    <div>
+      <Row type="flex" justify="center">
+          <div style={{height: "270px",width: '1245px',position:"relative",margin:"50px 0",overflow:"hidden"}}>
+          {
+            drawerData.map((item,index)=>{
+              return (
+                // 绝对定位
+                <div id={index} style={{height:"100%",position:"absolute",left:index*195 + 'px'}}>
+                  {/* 浮动定位 */}
+                  <div style={{float:"left"}} onMouseOver={()=> drawerClick(index)}>
+                    <img src={item.smallImg}/>
+                  </div>
+                  <img src={item.longImg}/>
+                </div>
+              )
+            })
+          }
+          </div>
+      </Row>
+    </div>
   )
 }
